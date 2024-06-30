79919fca4ff8e3772a047d7f3d962d6a3408ce36
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-07-01 00:34:37 +0500

    fix about us and resume

--- src/Pages/About/GitHubContribution.js
+++ src/Pages/About/GitHubContribution.js
@@ -53,7 +53,7 @@ const ContributionGraph = ({ username }) => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const token = "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"; // Replace with your GitHub token
+        const token = "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"; // Replace with your GitHub token
         const contributions = await fetchContributions(username, token);
         setContributions(contributions);
       } catch (err) {
